f9537a5a146d874184ee3a5d920c8033c2320d4b
szorfein <szorfein@gmail.com>
2017-02-06 19:15:33 +0100

    reuse new sidebar.component in article-detail

---
+++ client/imports/app/articles/article-sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+import template from './article-sidebar.component.html'
+
+@Component({
+    selector: 'sidebar',
+    template
+})
+
+export class ArticleSidebarComponent {
+    constructor() {}
+}
--- client/imports/app/articles/index.ts
+++ client/imports/app/articles/index.ts
@@ -2,10 +2,12 @@ import { ArticlesFormComponent } from './articles-form.component'
 import { ArticlesListComponent } from './articles-list.component'
 import { ArticleDetailsComponent } from './article-details.component'
 import { ArticlesListLastestComponent } from './articles-list-lastest.component'
+import { ArticleSidebarComponent } from './article-sidebar.component'
 
 export const ARTICLES_DECLARATIONS = [
     ArticlesFormComponent,
     ArticlesListComponent,
     ArticleDetailsComponent,
-    ArticlesListLastestComponent
+    ArticlesListLastestComponent,
+    ArticleSidebarComponent
 ]
